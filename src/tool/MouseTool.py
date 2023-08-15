e544cd245ec657888286c2052cd694f951496d0e
jiamao <liujiamao@163.com>
2023-08-16 00:13:37 +0800

    修改硬编码

--- src/tool/MouseTool.py
+++ src/tool/MouseTool.py
@@ -3,25 +3,26 @@ import win32con
 import win32api
 from ctypes import windll
 
+from src.constant import Constant
 from src.entity.Point import Point
 
 
 def move(pos:Point):
-    time.sleep(0.05)
+    time.sleep(Constant.OPERATE_DELAY_TIME)
     windll.user32.SetCursorPos(pos.x,pos.y)
-    time.sleep(0.05)
+    time.sleep(Constant.OPERATE_DELAY_TIME)
 
 def clickLeft():
-    time.sleep(0.05)
+    time.sleep(Constant.OPERATE_DELAY_TIME)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0,0,0)
-    time.sleep(0.05)
+    time.sleep(Constant.OPERATE_DELAY_TIME)
 
 def clickRight():
-    time.sleep(0.05)
+    time.sleep(Constant.OPERATE_DELAY_TIME)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0,0,0)
-    time.sleep(0.05)
+    time.sleep(Constant.OPERATE_DELAY_TIME)
 
 
 
